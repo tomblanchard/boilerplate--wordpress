16442c57f8c1955bbecb74730d078cdcbc5c3c01
Tom Blanchard <yo@tomblanchard.co.uk>
2014-09-14 12:46:41 +0100

    Stuff

--- content.php
+++ content.php
@@ -2,8 +2,8 @@
 
   <h1><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
 
-  <time datetime="<?php echo get_the_time('Y-m-d'); ?>">
-    <?php echo get_the_time(get_option('date_format')); ?>
+  <time datetime="<?php the_time('Y-m-d'); ?>">
+    <?php echo get_the_time( get_option('date_format') ); ?>
   </time>
 
   <?php the_post_thumbnail( '500x500', array('class' => '') ); ?>
