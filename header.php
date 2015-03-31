70be646c2eca83138677126f904346d590423576
Tom Blanchard <yo@tomblanchard.co.uk>
2015-03-31 21:51:14 +0100

    Stuff

--- header.php
+++ header.php
@@ -5,7 +5,7 @@
   <meta charset="<?php bloginfo( 'charset' ); ?>">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="description" content="<?php bloginfo( 'description' ); ?>">
-  <meta name="viewport" content="width=device-width">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
 
   <title><?php bloginfo( 'name' ); ?><?php wp_title( '-' ); ?></title>
 
